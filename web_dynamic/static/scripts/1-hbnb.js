72fdeaa3d73099714870ae34833f8f298b803d97
Matilop15 <3959@holbertonschool.com>
2022-05-27 11:03:28 -0300

    update tasks 3
    Co-authored-by: AngelPiva <AngelPiva@users.noreply.github.com>

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-    let dictAmenities = {}
-    $('input[type="checkbox"]').change(function () {
-        if ($(this).is(':checked')) {
-            dictAmenities[$(this).attr('data-id')] = $(this).attr('data-name');
-        } else {
-            delete dictAmenities[$(this).attr('data-id')];
-        }
-        $('DIV.amenities h4').text(Object.values(dictAmenities));
-    })
+$(document).ready(function () {
+  let dictAmenities = {}
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      dictAmenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete dictAmenities[$(this).attr('data-id')];
+    }
+    $('DIV.amenities h4').text(Object.values(dictAmenities).join(', '));
+});
 });
