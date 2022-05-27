5d94aa872fba4b6359139a369eb16461cc090f48
Matilop15 <3959@holbertonschool.com>
2022-05-27 10:15:42 -0300

    updateee

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     let dictAmenities = {}
     $('input[type="checkbox"]').change(function () {
-        if ($(this).is(':checked')) {
+        if ($(this).is(' :checked')) {
             dictAmenities[$(this).attr('data-id')] = $(this).attr('data-name');
         } else {
-            delete dictAmenities[$(this).attr('data-id')];
+            delete dictAmenities[$(this).attr('data-name')];
         }
         $('DIV.amenities h4').text(Object.values(dictAmenities));
     })
